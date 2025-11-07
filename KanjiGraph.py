94a4e9d3fd4999d99c53e2657c28837e4c5ab717
ferns5 <granttirrell0310@gmail.com>
2025-11-06 23:26:36 -0500

    built and populated graph object in main.py

--- KanjiGraph.py
+++ KanjiGraph.py
@@ -1,10 +1,15 @@
 class KanjiGraph:
   def __init__(self):
     self.graph = {}
-  def add_node(self, kanji: str):
-    if kanji not in self.graph:
-      self.graph[kanji] = {}
-  def add_edge(self, source: str, target: str, weight: int):
-    self.add_node(source)
-    self.add_node(target)
-    self.graph[source][target] = weight
+    self.nodes_data = {}
+    self.node_count = 0
+
+  def add_node(self, node_id, **kwargs):
+    if node_id not in self.graph:
+      self.graph[node_id] = {}
+      self.nodes_data[node_id] = kwargs
+      self.node_count+=1
+
+  def add_edge(self, source, target, weight):
+    if source in self.graph and target in self.graph:
+      self.graph[source][target] = weight
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 from jamdict import Jamdict
 from KanjiGraph import KanjiGraph
 
+# Entry Type Objects
 class KanjiEntry:
   def __init__(self, row):
     self.kanji = row['literal']
@@ -15,6 +16,7 @@ class VocabEntry:
     self.kanji_content = [char for char in self.text if '一' <= char <= '龯']
     self.log_cost = 20.0
 
+# Data Loading Helper Functions
 def load_data():
   print("Loading initial Kanji Dictionary Data...")
   jam = Jamdict()
@@ -72,31 +74,66 @@ def load_frequency_data(entries, filename='frequency_data.csv'):
           matched += 1
   print("Frequency data loaded successfully!")
 
+# Populate Graph Helper Function
+def build_graph(graph, entries):
+  print("Populating graph with loaded entries...")
+  kanji_nodes = {}
+
+  #Nodes
+  for entry in entries:
+    node_id = entry.kanji if isinstance(entry, KanjiEntry) else entry.text
+    graph.add_node(node_id, type="kanji" if isinstance(entry, KanjiEntry) else 'vocab')
+    if isinstance(entry, KanjiEntry):
+      kanji_nodes[entry.kanji] = entry
+  print(f"Added {graph.node_count} nodes to graph.")
+
+  #Edges
+  print("Creating edges and assigning weights by frequency...")
+  MAX_COST = 18.42
+  for entry in entries:
+    if isinstance(entry, VocabEntry):
+      word_node = entry.text
+      if entry.log_cost > MAX_COST:
+        continue
+      cost = entry.log_cost
+      for kanji_char in entry.kanji_content:
+        if kanji_char in kanji_nodes:
+          # add word to kanji edge
+          graph.add_edge(word_node, kanji_char, cost)
+          #add kanji to word edge
+          graph.add_edge(kanji_char, word_node, cost)
+  print(f"Edges created. Nodes used: {len(graph.graph)}")
+  return graph
 
 if __name__ == "__main__":
   jam, entries = load_data()
   load_frequency_data(entries)
+  empty_graph = KanjiGraph()
+  graph = build_graph(empty_graph, entries)
 
-  kanji_count = sum(1 for entry in entries if isinstance(entry, KanjiEntry))
-  def debug_check_data(entries, kanji_count):
-    print("verify kanji nodes:")
-    for i in range(5):
-      entry = entries[i]
-      print(f"Kanji Node {i+1}: {entry.kanji}, Strokes: {entry.stroke_count}")
+  #kanji_count = sum(1 for entry in entries if isinstance(entry, KanjiEntry))
+  #def debug_check_data(entries, kanji_count):
+  #  print("verify kanji nodes:")
+  #  for i in range(5):
+  #    entry = entries[i]
+  #    print(f"Kanji Node {i+1}: {entry.kanji}, Strokes: {entry.stroke_count}")
 
-    print("verify vocab nodes:")
-    checks = 0
-    for entry in entries:
-      if isinstance(entry, VocabEntry):
-        if entry.log_cost<20.0:
-          kanji_list = ", ".join(entry.kanji_content)[:3]+"..." if len(entry.kanji_content) > 3 else ", ".join(entry.kanji_content)
-          freq_status = "LOW" if entry.log_cost > 10 else "HIGH"
-          print(f" Word: {entry.text} Cost: {entry.log_cost:.4f}, Frequency Status: {freq_status}, Kanji: {kanji_list}")
-          checks+=1
-          if checks >= 20:
-            break
-    if checks == 0:
-      print("No loaded frequency found")
+  #  print("verify vocab nodes:")
+  #  checks = 0
+  #  for entry in entries:
+  #    if isinstance(entry, VocabEntry):
+  #      if entry.log_cost<20.0:
+  #        kanji_list = ", ".join(entry.kanji_content)[:3]+"..." if len(entry.kanji_content) > 3 else ", ".join(entry.kanji_content)
+  #        freq_status = "LOW" if entry.log_cost > 10 else "HIGH"
+  #        print(f" Word: {entry.text} Cost: {entry.log_cost:.4f}, Frequency Status: {freq_status}, Kanji: {kanji_list}")
+  #        checks+=1
+  #        if checks >= 20:
+  #          break
+  #  if checks == 0:
+  #    print("No loaded frequency found")
 
-  debug_check_data(entries, kanji_count)
+  #debug_check_data(entries, kanji_count)
+
+  print(f"verification: Total Nodes: {graph.node_count}")
+  print(f"Example Edge: (from 明白) {graph.graph.get('明白')}")
   graph = KanjiGraph()
